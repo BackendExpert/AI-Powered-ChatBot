21d21256a34f9d4cdd4f9a883d34eaf5e345e43c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-22 08:35:09 +0530

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HomePage = () => {
+    const [SearchData, SetSearchData] = useState("")
+    const [ResultData, SetResultData] = useState([])
+
+    const headleSearch = async() => {
+        const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+        const cx = process.env.REACT_APP_CUSTOM_SEARCH_ENGINE_ID;
+
+        if (!apiKey || !cx) {
+            alert("unknown Error white Searching Data....")
+            return;
+        }
+
+        try {
+            const response = await axios.get(
+              `https://www.googleapis.com/customsearch/v1?q=${SearchData}&key=${apiKey}&cx=${cx}`
+            );
+            SetResultData(response.data.items || []);
+        } catch (error) {
+            console.error("Error fetching search results:", error);
+        }
+    }
   return (
     <div>HomePage</div>
   )
